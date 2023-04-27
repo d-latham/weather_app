0ba6d7c8847011a7ace826e703f06eaff264afc6
Rakesh Janapureddy <123701972+Rakeshh25@users.noreply.github.com>
2023-04-27 20:00:16 +0000

    Astro

--- WeatherApp/Clients/WeatherClient.cs
+++ WeatherApp/Clients/WeatherClient.cs
@@ -3,52 +3,63 @@ using WeatherApp.Models;
 
 namespace WeatherApp.Clients;
 
-public class WeatherClient {
+public class WeatherClient
+{
     private readonly HttpClient _client;
 
-    public WeatherClient(HttpClient client) {
+    public WeatherClient(HttpClient client)
+    {
         _client = client;
     }
 
-    public async Task<WeatherResponse> getWeather (string pageaddress,string location, string numberOfDays,string date) {
+    public async Task<WeatherResponse> getWeather(string pageaddress, string location, string numberOfDays, DateTime? date)
+    {
 
         string pageA = pageaddress;
         string api_key = "d5f4a95b8c7a4c6eb1a125959231404";
-        string url = "http://api.weatherapi.com/v1/"+pageA+".json?key=" + api_key;
-        string paramaters ="&aqi=yes";
+        string url = "https://api.weatherapi.com/v1/" + pageA + ".json?key=" + api_key;
+        string paramaters = "&aqi=yes";
         string numDays = "3";
         string dateDefault = "2023-04-15";
-        
-        
 
-        if (location != null) {
+
+
+        if (location != null)
+        {
             url += "&q=" + location;
-        } 
-        else {
+        }
+        else
+        {
             url += "&q=81301";
         }
-        if (numberOfDays != null) {
+        if (numberOfDays != null)
+        {
             numDays = numberOfDays;
         }
+
         
-        if(date!=null){
-dateDefault = date;
+
+        if (pageA == "current")
+        {
+            url += paramaters;
+        }
+        else if (pageA == "forecast")
+        {
+            paramaters = "&days=" + numDays + "&aqi=yes&alerts=yes" ;
+            if (date.HasValue)
+            {
+                paramaters = paramaters + "&dt="  + date?.ToString("yyyy-MM-dd");
+            }
+            url += paramaters;
+        }
+        else
+        {
+            paramaters = "&dt=" + date;
+            url += paramaters;
         }
 
-        if(pageA == "current"){
-url += paramaters;
-        }
-        else if(pageA == "forecast"){
-            paramaters = "&days="+numDays+"&aqi=yes&alerts=yes";
-            url += paramaters;
-        }
-        else{
-            paramaters = "&dt="+date;
-            url += paramaters;
-        }
-        
         Console.WriteLine(url);
         return await _client.GetFromJsonAsync<WeatherResponse>(url);
-        
+
     }
 }
